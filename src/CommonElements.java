be751960d63f5d2b8cf9c6610e5a4dfaf51879fd
honey1234567 <42098471+honey1234567@users.noreply.github.com>
2023-05-09 18:02:19 +0530

    Add files via upload

---
+++ src/CommonElements.java
@@ -0,0 +1,70 @@
+import java.util.HashMap;
+//Given two integer arrays, A and B of size N and M, respectively. Your task is to find all the common elements in both the array.
+//
+//        NOTE:
+//
+//        Each element in the result should appear as many times as it appears in both arrays.
+//        The result can be in any order.
+//
+//
+//        Problem Constraints
+//        1 <= N, M <= 105
+//
+//        1 <= A[i] <= 109
+//
+//
+//
+//        Input Format
+//        First argument is an integer array A of size N.
+//
+//        Second argument is an integer array B of size M.
+//
+//
+//
+//        Output Format
+//        Return an integer array denoting the common elements.
+//
+//
+//
+//        Example Input
+//        Input 1:
+//
+//        A = [1, 2, 2, 1]
+//        B = [2, 3, 1, 2]
+//        Input 2:
+//
+//        A = [2, 1, 4, 10]
+//        B = [3, 6, 2, 10, 10]
+//
+//
+//        Example Output
+//        Output 1:
+//
+//        [1, 2, 2]
+//        Output 2:
+//
+//        [2, 10]
+
+// https://www.youtube.com/watch?v=jMwLFkI2Zbc
+public class CommonElements {
+    public void solve(int[] A, int[] B) {
+        int l1 = A.length;
+        int l2 = B.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < l1; i++) {
+            int freq = map.getOrDefault(A[i], 0);
+            int newf = freq + 1;
+            map.put(A[i], newf);
+        }
+        for (int i = 0; i < l2; i++) {
+
+            int freq = map.containsKey(B[i]) ? map.get(B[i]) : 0;
+            if (freq > 0) {
+
+                int newf = freq - 1;
+                map.put(B[i], newf);
+                System.out.println(B[i]);
+            }
+        }
+    }
+}
---
+++ src/PowerFunction.java
@@ -0,0 +1,63 @@
+//Problem Constraints
+//        -109 <= A <= 109
+//        0 <= B <= 109
+//        1 <= C <= 109
+//
+//
+//        Input Format
+//        Given three integers A, B, C.
+//
+//
+//        Output Format
+//        Return an integer.
+//
+//
+//        Example Input
+//        Input 1:
+//        A = 2
+//        B = 3
+//        C = 3
+//        Input 2:
+//
+//        A = 3
+//        B = 3
+//        C = 1
+//
+//
+//        Example Output
+//        Output 1:
+//        2
+//        Output 2:
+//
+//        0
+//
+//
+//        Example Explanation
+//        Explanation 1:
+//        23 % 3 = 8 % 3 = 2
+//        Explanation 2:
+//
+//        33 % 1 = 27 % 1 = 0
+
+
+public class PowerFunction {
+    public long fastPower(long a ,long b,long c){
+        long res = 1;
+        while(b>0){
+            if((b & 1) != 0 ){//if b is odd
+                res = (res % c * a % c) % c;//last 2/2 is 1 even for even no. so control goes here and u'll get latest value
+            }
+            a = (a % c* a % c)%c;
+            b=b>>1;//b/2
+        }
+        return res;
+    }
+//we will multiply at each step as a^2 mod n = (a*a)mod n=(a mod n * a mod n)mod n
+    //doing mod on small value is less computive task that's why we used formula
+    //use bitwise operator coz it takes less time than multiplication and division
+   // here complexity is o(logb)in binary search  else if u do simply multiolication then o(b)
+    //we ttok long coz if a,b,c takes higher int range then int result can't be fitted with answer,so res is long
+
+
+    //https://youtu.be/CnPreli2F-E
+}
