c9daec753a5f3f7e34cb58af63d3d86b25afc0c8
honey1234567 <42098471+honey1234567@users.noreply.github.com>
2023-05-16 18:41:45 +0530

    Update Paircount.java

--- src/Paircount.java
+++ src/Paircount.java
@@ -19,7 +19,7 @@ public class Paircount {
     public int solve(int[] A, int B) {
         int count = 0;
         for(int i = 0;i<A.length;i++){
-            for(int j = i+1;j<A.length;j++){
+            for(int j = i+1;j<A.length;j++){//start from i+1,coz it has already amde pair with previous
                 if((A[i]+ A[j]) == B){
                     count++;
                 }
